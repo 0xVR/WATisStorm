a31f6d81851825ef16bd50f9f7071a0b5fc1ec28
Hadi Zaidi <38956768+0xVR@users.noreply.github.com>
2024-01-03 14:09:26 -0500

    Fix array parsing bug

--- api/MapParser.ts
+++ api/MapParser.ts
@@ -1,32 +1,25 @@
-import { Graph } from './Graph';
+import { Graph, Node } from './Graph';
 import * as fs from 'fs';
 import * as path from 'path';
 
 export const parseMap = async (graph: Graph) => {
     const data = await fs.promises.readFile(path.resolve(__dirname, "../assets/map.txt"), "utf8");
     data.split('\n').forEach(e => {
+        e = e.trim()
         if (e.startsWith("building")) {
             // Add node
             const [id, name] = e.substring(e.indexOf(' ') + 1).split('-');
             graph.addNode(id, name);
         } else if (e.startsWith("connection")) {
             // Add edge
-            console.log(graph.nodes);
-            const ids = e.substring(e.indexOf(' ') + 1).split(' ')
-            ids.map(n => {
-                if (graph.nodes.has(n)) {
-                    return graph.nodes.get(n);
-                } else {
-                    throw new Error("Undefined connection!");
-                }
+            const ids = e.substring(e.indexOf(' ') + 1).split(' ').map((n): Node => {
+                const node = graph.nodes.get(n);
+                if (!node) throw new Error("Undefined connection");
+                return node;
             })
-            console.log(ids);
-            /*
             const [source, destination] = ids;
             source.addAdjacent(destination);
-            destination.addAdjacent(source);*/
+            destination.addAdjacent(source);
         }
     })
 }
-
-parseMap(new Graph());
