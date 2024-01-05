ee3e3a1023d65b1f9ee2cdc2b5b5499263751430
Hadi Zaidi <38956768+0xVR@users.noreply.github.com>
2024-01-05 00:39:39 -0500

    Restructure objects to allow for easier pathfinding

--- api/RouteFinder.ts
+++ api/RouteFinder.ts
@@ -1,17 +1,26 @@
 import { Graph, Node } from "./Graph"
 
-const findClosestNode = (distances: {node: Node, weight: number}[], visited: Node[]): Node | undefined => {
+const findClosestNode = (distances: {[nodeid: string]: {node: Node, weight: number}}, visited: Node[]): Node | undefined => {
     let closest: {node: Node, weight: number} | undefined;
-    distances.forEach(node => {
-        if (!closest || node.weight < closest.weight) {
-            if (!visited.includes(node.node)) {
-                closest = node;
+    for (const node in distances) {
+        if (!closest || distances.node.weight < closest.weight) {
+            if (!visited.includes(distances.node.node)) {
+                closest = distances.node;
             }
         }
-    })
+    }
     return closest?.node;
 }
 
 export const findRoute = (graph: Graph, start: Node, end: Node) => {
-    //
+    let distances: {[nodeid: string]: {node: Node, weight: number}} = {};
+    distances[end.id] = {node: end, weight: Infinity};
+    start.adjacents.forEach(n => {distances[n.node.id] = n});
+
+    let parents: {[nodeid: string]: Node | null} = {};
+    parents[end.id] = null;
+    start.adjacents.forEach(n => parents[n.node.id] = start);
+
+    let visited: Node[] = [];
+    let node = findClosestNode(distances, visited);
 }
